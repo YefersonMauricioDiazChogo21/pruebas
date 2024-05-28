5ebe1476ad2d657d0a0b7ece6f305027bf65b768
YefersonMauricioDiazChogo21 <yefersondiaz2001@gmail.com>
2024-05-28 17:34:11 -0500

    feat :tada: start

---
+++ interfaz.js
@@ -0,0 +1,13 @@
+Int.innerHTML=`
+<nav class="navbar ">
+<div class="container-fluid">
+    <div class="d-flex flex-column align-items-center justify-content-center">
+        <a class="navbar-brand">Confecciones Pepita</a>
+        <img class="logo" src="img/logo.png" alt="">
+    </div>
+  <form class="d-flex search" role="search">
+    <button class="btn btn-outline-dark" type="submit">Salir</button>
+  </form>
+</div>
+</nav>
+`
